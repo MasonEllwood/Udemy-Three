21358e26922065997d738e8d0732e1f1246f9a75
Mason Ellwood <masonellwood@gmail.com>
2021-03-31 09:31:36 -0700

    helper added

--- src/index.js
+++ src/index.js
@@ -12,15 +12,22 @@ class App extends React.Component {
         );
     }
 
-    // we have to define render
-    render() {
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>;
         } else if (!this.state.errorMessage && this.state.lat) {
             return <SeasonDisplay lat={this.state.lat} />
         } else {
-            return <Spinner />;
+            return <Spinner message="Please accept location request"/>;
         }
+    }
+
+    render() {
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        );
     } 
 }
 
